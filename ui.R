d71330550e0c16bc822454978959149468ff92d0
gdaem1 <47893424+gdaem1@users.noreply.github.com>
2020-03-16 15:56:32 -0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -11,117 +11,117 @@ shinyUI(
           'Total Revenue for Control:',
           min = 0,
           max = 1e6,
-          value = 4675.13
+          value = 7760.50
         ),
         numericInput(
           'success_A',
           'Control Conversions:',
           min = 0,
           max = 1e6,
-          value = 405
+          value = 349
         ),
         numericInput(
           'total_A',
           'Total Control Players:',
           min = 0,
           max = 1e6,
-          value = 32597
+          value = 1490
         ),
         numericInput(
           'retention_A',
-          'retention A:',
+          'Retention A:',
           min = 0,
           max = 1e6,
-          value = 200
+          value = 699
         ),
         numericInput(
           'rev_B',
           'Total Revenue Treatment 1:',
           min = 0,
           max = 1e6,
-          value = 2534.80
+          value = 9322.70
         ),
         numericInput(
           'success_B',
           'Converted Players Treatment 1:',
           min = 0,
           max = 1e6,
-          value = 212
+          value = 386
         ),
         numericInput(
           'total_B',
           'Total Players Treatment 1:',
           min = 0,
           max = 1e6,
-          value = 16437
+          value = 1614
         ),
         numericInput(
           'retention_B',
-          'retention B:',
+          'Retention B:',
           min = 0,
           max = 1e6,
-          value = 230
+          value = 794
         ),
         numericInput(
           'rev_C',
           'Total Revenue Condition 2:',
           min = 0,
           max = 1e6,
-          value = 2606.49
+          value = 7602.54
         ),
         numericInput(
           'success_C',
           'Converted Players Condition 2:',
           min = 0,
           max = 1e6,
-          value = 227
+          value = 329
         ),
         numericInput(
           'total_C',
           'Total Players in Condition 2:',
           min = 0,
           max = 1e6,
-          value = 16199
+          value = 1501
         ),
         numericInput(
           'retention_C',
-          'retention C:',
+          'Retention C:',
           min = 0,
           max = 1e6,
-          value = 230
+          value = 750
         ),
         numericInput(
           'rev_D',
           'Total Revenue Condition 3:',
           min = 0,
           max = 1e6,
-          value = 2606.49
+          value = 8770.27
         ),
         numericInput(
           'success_D',
           'Converted Players Condition 3:',
           min = 0,
           max = 1e6,
-          value = 227
+          value = 337
         ),
         numericInput(
           'total_D',
           'Total Players in Condition 3:',
           min = 0,
           max = 1e6,
-          value = 16199
+          value = 1582
         ),
         numericInput(
           'retention_D',
-          'retention D:',
+          'Retention D:',
           min = 0,
           max = 1e6,
-          value = 230
+          value = 832
         ),
         numericInput(
           'sim_sample',
-          'Monte-Carlo Simulation Samples:',
-          min = 2,
+          'Monte-Carlo MC Samples:',
+          min = 1,
           max = 1e7,
           value = 1e5
         ),
